1fc047a4adf778efc2a1cf9bc65132af7affbb6d
Aurélie Violette <orwel.violette@gmail.com>
2025-01-20 16:43:29 +0100

    Add local edition for cell

--- src/TableCell.vue
+++ src/TableCell.vue
@@ -1,10 +1,11 @@
 <template>
-    <wwLocalContext :data="{ index, column, data }" elementKey="cell">
+    <wwLocalContext :data="{ index, column, data, isEditing, editingData }" :methods="localMethods" elementKey="cell">
         <wwElement v-bind="cellElement" tag="td" :wwProps="{ overrideDisplayValues: ['table-cell'] }"></wwElement>
     </wwLocalContext>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
     props: {
         index: { type: Number, required: true },
@@ -14,8 +15,64 @@ export default {
     },
     setup() {
         const { resolveMappingFormula } = wwLib.wwFormula.useFormula();
+
+        const isEditing = ref(false);
+        const editingData = ref(null);
+
+        function toggleEditing() {
+            isEditing.value = !isEditing.value;
+        }
+        function setIsEditing(value) {
+            isEditing.value = value;
+        }
+        function setEditingData(data) {
+            editingData.value = data;
+        }
+
         return {
             resolveMappingFormula,
+            isEditing,
+            editingData,
+            localMethods: {
+                toggleEditing: {
+                    method: toggleEditing,
+                    editor: {
+                        label: 'Toggle Editing',
+                        description: 'Toggle the editing of the cell',
+                        group: 'Datagrid cell',
+                    },
+                },
+                setIsEditing: {
+                    method: setIsEditing,
+                    editor: {
+                        label: 'Set Is Editing',
+                        description: 'Set the editing of the cell',
+                        group: 'Datagrid cell',
+                        args: [
+                            {
+                                name: 'value',
+                                type: 'boolean',
+                                required: true,
+                            },
+                        ],
+                    },
+                },
+                setEditingData: {
+                    method: setEditingData,
+                    editor: {
+                        label: 'Set Editing data',
+                        description: '',
+                        group: 'Datagrid cell',
+                        args: [
+                            {
+                                name: 'value',
+                                type: 'any',
+                                required: true,
+                            },
+                        ],
+                    },
+                },
+            },
         };
     },
     computed: {
